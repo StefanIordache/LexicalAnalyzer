00f1631cfe291a5828943469bfab528bacabdf9d
StefanIordache <iordachestefan1@gmail.com>
2019-03-06 20:56:17 +0200

    View updates

--- LexicalAnalyzer/Analyzer/urls.py
+++ LexicalAnalyzer/Analyzer/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from Analyzer.views import index
+from Analyzer.views import analyze
 from django.conf.urls import (
     handler400, handler403, handler404, handler500
 )
 
 urlpatterns = [
+    url(r'^analyze/$', analyze, name='analyze'),
     url(r'^', index, name='index'),
 ]
--- LexicalAnalyzer/Analyzer/views.py
+++ LexicalAnalyzer/Analyzer/views.py
@@ -3,14 +3,23 @@ from __future__ import unicode_literals
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login, logout
+from django.http.response import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
 def index(request):
-    import sys
-    if sys.version_info[0] < 3:
-        raise Exception("Please switch to Python 3 versions")
-    else:
-        print(sys.version_info[0])
     return render(request, 'Analyzer/analyzer.html')
+
+
+@csrf_exempt
+def analyze(request):
+    if request.POST:
+        selected_language = request.POST['language']
+        input_code = request.POST['code']
+        print('ceva')
+        return HttpResponse(selected_language, content_type="application/json")
+    else:
+        return render(request, 'Analyzer/analyzer.html')
+
