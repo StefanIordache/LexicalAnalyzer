6bbdb032e8d4ae2005269de3364ef6916c3308ed
StefanIordache <iordachestefan1@gmail.com>
2019-03-06 17:28:13 +0200

    Small fix

--- LexicalAnalyzer/Analyzer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from Analyzer.views import index
-from django.conf.urls import (
-    handler400, handler403, handler404, handler500
-)
-
-urlpatterns = [
-    url(r'^', index, name='index'),
-]
